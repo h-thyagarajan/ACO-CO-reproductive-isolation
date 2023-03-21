19daff2dc2533b40255a424135d31025b119a5d2
h-thyagarajan <harshavardhanthyagarajan@gmail.com>
2023-03-21 16:56:41 -0400

    Data summaries

--- R Scripts/Latency_Duration_MixedModels.R
+++ R Scripts/Latency_Duration_MixedModels.R
@@ -18,6 +18,7 @@ str(fmatechoicedat)
 f.lat.int.mix.mod <- lmer(log(mating.latency+1) ~ mated.male.pop*female.pop + (1|trial.id/replicate), data=fmatechoicedat)
 anova(f.lat.int.mix.mod)
 emmeans(f.lat.int.mix.mod, pairwise~female.pop*mated.male.pop)
+f.lat<-fmatechoicedat %>% group_by(female.pop, mated.male.pop) %>% summarize(dur.avg=mean(mating.latency), dur.sd=sd(mating.latency));f.lat
 
 #Assumption testing
 plot(resid(f.lat.int.mix.mod))
@@ -27,6 +28,7 @@ hist(resid(f.lat.int.mix.mod))
 f.dur.int.mix.mod <- lmer(mating.duration ~ mated.male.pop*female.pop + (1|trial.id/replicate), data=fmatechoicedat)
 anova(f.dur.int.mix.mod)
 emmeans(f.dur.int.mix.mod, pairwise~female.pop*mated.male.pop)
+f.dur<-fmatechoicedat %>% group_by(female.pop, mated.male.pop) %>% summarize(lat.avg=mean(mating.duration), lat.sd=sd(mating.duration));f.dur
 
 #Assumption testing
 plot(resid(f.dur.int.mix.mod))
@@ -44,6 +46,7 @@ ggplot(data=fmatechoicedat, aes(x=female.pop, y=mating.duration, fill=mated.male
 #data summary
 f.dur<-fmatechoicedat %>% group_by(female.pop) %>% summarize(dur.avg=mean(mating.duration), dur.sd=sd(mating.duration));f.dur
 
+
 ##MALE DATA SECTION##
 #input data+data structure+filter out NAs
 mmatechoicedat <- read.csv(file.choose())
@@ -54,6 +57,7 @@ mmatechoicedat$replicate <- as.numeric(gsub("[AC]", "", mmatechoicedat$Mated.mal
 m.lat.int.mix.mod <- lmer(log(mating.latency+1) ~ Mated.male.pop*Mated.female.pop + (1|Trial.ID/replicate), data=mmatechoicedat)
 anova(m.lat.int.mix.mod)
 emmeans(m.lat.int.mix.mod, pairwise~Mated.male.pop*Mated.female.pop)
+m.lat<-mmatechoicedat %>% group_by(Mated.female.pop, Mated.male.pop) %>% summarize(lat.avg=mean(mating.latency), lat.sd=sd(mating.latency));m.lat
 
 #Assumption testing
 plot(resid(m.lat.int.mix.mod))
@@ -63,6 +67,7 @@ hist(resid(m.lat.int.mix.mod))
 m.dur.int.mix.mod <- lmer(mating.duration ~ Mated.male.pop*Mated.female.pop + (1|Trial.ID/replicate), data=mmatechoicedat)
 anova(m.dur.int.mix.mod)
 emmeans(m.dur.int.mix.mod, pairwise~Mated.male.pop*Mated.female.pop)
+m.dur<-mmatechoicedat %>% group_by(Mated.female.pop, Mated.male.pop) %>% summarize(dur.avg=mean(mating.duration), dur.sd=sd(mating.duration));m.dur
 
 #Assumption testing
 plot(resid(m.dur.int.mix.mod))
