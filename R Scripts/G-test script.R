5edae5b374f32543c3197256f764d71030090f2a
h-thyagarajan <harshavardhanthyagarajan@gmail.com>
2023-04-25 14:00:58 -0400

    .

--- R Scripts/G-test script.R
+++ R Scripts/G-test script.R
@@ -13,37 +13,23 @@ dat<-read.csv(file.choose()) #choose "female choice- compiled data.csv"
 ## start with A1 females- determine the number of A and C males females mated with in each trial
 
 dat1<- dat %>% filter(female.id== "A1")
-table(dat1$female.id, dat1$mated.male.pop, dat1$trial.id)
+Example.Mat = t(as.matrix(table(dat1$female.id, dat1$mated.male.pop, dat1$trial.id)[1,,]))
 
-## step 1:  Individual G-value- fill in observed values (observed # A's mated, observed # C's mated) and expected proportions (0.5, 0.5) under null hypothesis
+## step 1:  Individual G-value
 # repeat this for each trial (1-4)
-
 #example for A1 female trial 1
 
-observed =c(21, 26)
+observed =Example.Mat[1,]
 expected =c(0.5,0.5)
 GTest(x=observed, p=expected, correct="none") #output gives individual G value, df, p-value (testing for deviation from expected proportion)
 
 #repeat this for A1 female trials 2, 3 and 4. 
+## step 2: G-test of independence
 
-## step 2: G-test of independence- fill in observed values across all four trial for homotypic and heterotypic matings
+GTest(Example.Mat, correct="none") 
+#output gives heterogeneity G value, df, p-value (testing for significant heterogeneity between trials)
 
-Input =("
- Trial   Homo   Hetero       
-  1       21     26
-  2       30     18
-  3       22     21
-  4       29     20
-        ")
-
-Matriz = as.matrix(read.table(textConnection(Input),
-                              header=TRUE, 
-                              row.names=1)) ; Matriz
-
-GTest(Matriz,
-      correct="none") # output gives heterogeneity G value, df, p-value (testing for significant heterogeneity between trials)
-
-## step 3: Pooled G-value- fill in values pooled across replicates and expected proportions under null hypothesis
+## step 3: Pooled G-value- use values pooled across replicates and expected proportions under null hypothesis
 
 observed =c(102, 85)
 expected =c(0.5,0.5)
@@ -54,7 +40,7 @@ GTest(x=observed, p=expected, correct="none") # output gives pooled G value, df,
 # G-value= Heterogeneity G + Pooled G
 # df= df (heterogeneity) + df (pooled)
 # p-value calculated using CHIDIST function in excel, filling in total g-value and df
-   # this p-value tests for overall deviation from null hypothesis across replicates
+# this p-value tests for overall deviation from null hypothesis across replicates
 
 ###########################################################################
 
